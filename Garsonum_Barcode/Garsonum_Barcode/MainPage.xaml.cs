c230479dd059a8b3267589cd20968030bee78b3e
Garsonum <appgarsonum@gmail.com>
2019-03-01 13:45:53 +0300

    First Commit

---
+++ Garsonum_Barcode/Garsonum_Barcode/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+
+namespace Garsonum_Barcode
+{
+    public partial class MainPage : ContentPage
+    {
+        ZXingScannerPage scanPage;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Btn_Scn_OnClicked(object sender, EventArgs e)
+        {
+            scanPage = new ZXingScannerPage();
+            scanPage.OnScanResult += (result) => {
+                scanPage.IsScanning = false;
+
+                Device.BeginInvokeOnMainThread(() => {
+                    Navigation.PopAsync();
+                    DisplayAlert("Scanned Barcode", result.Text, "OK");
+                });
+            };
+
+            await Navigation.PushAsync(scanPage);
+        }
+    }
+}
